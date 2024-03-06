b881694bb6ee3fba7f22dcc0ca534af0ec2008cf
Brittany Dugger <brittanydugger@Brittanys-MBP.attlocal.net>
2024-03-06 15:12:03 -0600

    removed component file

---
+++ src/files/SignUpForm.jsx
@@ -0,0 +1,54 @@
+import {useState} from 'react'
+
+function SignUpForm (){
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState(null)
+  
+
+  async function handleSubmit(event){
+    event.preventDefault();
+   try{
+    const response = await fetch('https://fsa-jwt-practice.herokuapp.com/signup')
+    const json = await response.json()
+    
+
+
+
+   }catch(error) {
+    setError(error.message);
+   }
+    
+
+
+  }
+
+
+
+  return (
+    <>
+    <h2>Sign Up</h2>
+    {error && <p>{error}</p>}
+    <form onSubmit={handleSubmit}>
+      <label>
+          Username: 
+       <input 
+        value={username} 
+        onChange={(e) => setUsername(e.target.value)}/>
+      </label>
+      
+      <label>
+          Password: 
+      <input 
+        value={password} 
+        onChange={(e) => setPassword(e.target.value)}/>
+      </label>
+    
+      <button type='submit'> SUBMIT </button>
+    </form>
+    </>
+  )
+}
+
+
+export default SignUpForm
